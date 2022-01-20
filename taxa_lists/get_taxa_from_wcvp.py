a05a2a1dfac66590e067d60613a0306b45179358
alrichardbollans <adamrb@protonmail.com>
2022-01-20 20:13:23 +0000

    Fix output

--- taxa_lists/get_taxa_from_wcvp.py
+++ taxa_lists/get_taxa_from_wcvp.py
@@ -8,7 +8,7 @@ inputs_path = resource_filename(__name__, 'inputs')
 outputs_path = resource_filename(__name__, 'outputs')
 
 
-def load_wcvp(families_of_interest=None, output_csv=None) -> pd.DataFrame:
+def get_accepted_taxa(families_of_interest=None, output_csv=None) -> pd.DataFrame:
     # Manually downloaded from http://sftp.kew.org/pub/data-repositories/WCVP/
     wcvp_input_CSV = os.path.join(inputs_path, 'wcvp_v7_dec_2021.txt')
     wcvp_data = pd.read_csv(wcvp_input_CSV, sep='|')
@@ -17,16 +17,16 @@ def load_wcvp(families_of_interest=None, output_csv=None) -> pd.DataFrame:
 
     wcvp_data = wcvp_data[wcvp_data['taxonomic_status'] == 'Accepted']
 
-    if output_csv is None:
-        wcvp_data.to_csv(os.path.join(outputs_path, 'wcvp_accepted_taxa.csv'))
-    else:
+    if output_csv is not None:
         wcvp_data.to_csv(output_csv)
 
     return wcvp_data
 
 
 def main():
-    load_wcvp()
+    get_accepted_taxa(output_csv=os.path.join(outputs_path, 'wcvp_accepted_taxa.csv'))
+    get_accepted_taxa(families_of_interest=['Apocynaceae', 'Rubiaceae'],
+                      output_csv=os.path.join(outputs_path, 'wcvp_accepted_taxa_apocynaceae_rubiaceae.csv'))
 
 
 if __name__ == '__main__':
