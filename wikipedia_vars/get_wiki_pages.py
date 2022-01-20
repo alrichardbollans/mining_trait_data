fc50f5dd270021b641d98f1178eb9f7e5300a34f
alrichardbollans <adamrb@protonmail.com>
2022-01-20 20:12:32 +0000

    Extend species list to whole family

--- wikipedia_vars/get_wiki_pages.py
+++ wikipedia_vars/get_wiki_pages.py
@@ -5,20 +5,20 @@ from pkg_resources import resource_filename
 
 import wikipedia_searches
 ### Inputs
-
-
-inputs_path = resource_filename(__name__, '../inputs')
-species_csv = os.path.join(inputs_path, 'standardised_order.csv')
+from taxa_lists.get_taxa_from_wcvp import get_accepted_taxa
 
 ### Outputs
 output_path = resource_filename(__name__, 'outputs')
 output_wiki_csv = os.path.join(output_path, 'list_plants_with_wiki_pages.csv')
 
 def main():
-    species_data = pd.read_csv(species_csv)
-    species_data.set_index('Accepted_Name', inplace=True)
+    accepted_data = get_accepted_taxa(families_of_interest=['Apocynaceae', 'Rubiaceae'])
 
-    species_list = species_data.index
+    ranks_to_use = ["SPECIES", "VARIETY", "SUBSPECIES"]
+
+    accepted_taxa = accepted_data.loc[accepted_data["rank"].isin(ranks_to_use)]
+
+    species_list = accepted_taxa["taxon_name"].values
 
     wikipedia_searches.make_wiki_hit_df(species_list, output_wiki_csv)
 
