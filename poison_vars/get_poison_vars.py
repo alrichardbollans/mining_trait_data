8abd4dd316d1d97961b888b0dc0192337f749e3c
alrichardbollans <adamrb@protonmail.com>
2022-01-21 12:10:30 +0000

    Include useful plants

--- poison_vars/get_poison_vars.py
+++ poison_vars/get_poison_vars.py
@@ -13,10 +13,12 @@ inputs_path = resource_filename(__name__, 'inputs')
 input_species_csv = os.path.join(inputs_path, 'clean.csv')
 littox1_csv = os.path.join(inputs_path, 'littox_matched_1.csv')
 littox2_csv = os.path.join(inputs_path, 'littox_matched_2.csv')
+useful_plants_file = os.path.join(inputs_path, 'useful_plant_processed_db.txt')
 
 ### Temp outputs
 temp_outputs_path = resource_filename(__name__, 'temp_outputs')
 littox_temp_output_accepted_csv = os.path.join(temp_outputs_path, 'littox_accepted.csv')
+useful_temp_output_accepted_csv = os.path.join(temp_outputs_path, 'useful_accepted.csv')
 powo_search_temp_output_csv = os.path.join(temp_outputs_path, 'powo_poisons.csv')
 powo_search_temp_output_accepted_csv = os.path.join(temp_outputs_path, 'powo_poisons_accepted.csv')
 
@@ -24,6 +26,28 @@ powo_search_temp_output_accepted_csv = os.path.join(temp_outputs_path, 'powo_poi
 output_path = resource_filename(__name__, 'outputs')
 output_poison_csv = os.path.join(output_path, 'list_of_poisonous_plants.csv')
 
+
+def prepare_useful_plants_poisons() -> pd.DataFrame:
+    useful_db = pd.read_csv(useful_plants_file, encoding='latin_1',sep='\t')
+
+    useful_db = useful_db[useful_db['Poisons'] == 1]
+
+    useful_db.rename(
+        columns={'acc_ipniid': 'Accepted_ID', 'binomial_acc_name': 'Accepted_Name'},
+        inplace=True)
+    useful_db['Accepted_Rank'] = 'Species'
+    useful_db['Accepted_Species'] = useful_db['Accepted_Name']
+
+    useful_db.dropna(subset=['Accepted_ID'],inplace=True)
+
+    # Drop columns containing 'Source' as this gets confused when compiling all data
+    cs = [c for c in useful_db.columns if 'Source' in c]
+    useful_db.drop(columns=cs,inplace=True)
+    # Then add a source column
+    useful_db['Source'] = 'Useful Plants Data'
+
+    useful_db.to_csv(useful_temp_output_accepted_csv)
+    return useful_db
 
 def prepare_littox_poisons() -> pd.DataFrame:
     # Due to size of littox csv files, cannot use R script to match names
@@ -53,11 +77,13 @@ def get_powo_poisons():
 
 
 def main():
-    get_powo_poisons()
-    prepare_littox_poisons()
+    # get_powo_poisons()
+    # prepare_littox_poisons()
+    prepare_useful_plants_poisons()
     powo_hits = pd.read_csv(powo_search_temp_output_accepted_csv)
     littox_hits = pd.read_csv(littox_temp_output_accepted_csv)
-    compile_hits([powo_hits, littox_hits], output_poison_csv)
+    useful_hits = pd.read_csv(useful_temp_output_accepted_csv)
+    compile_hits([useful_hits,powo_hits, littox_hits], output_poison_csv)
 
 
 if __name__ == '__main__':
