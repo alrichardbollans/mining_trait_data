6b4e5099d7f801eb807bb1515385fe76e4647991
alrichardbollans <adamrb@protonmail.com>
2022-01-25 12:59:00 +0000

    Add some hair traits (need to fix powo searches)

--- morphological_vars/get_morphological_vars.py
+++ morphological_vars/get_morphological_vars.py
@@ -20,13 +20,68 @@ temp_outputs_path = resource_filename(__name__, 'temp_outputs')
 
 spine_temp_output_cleaned_csv, spine_temp_output_accepted_csv = generate_temp_output_file_paths(
     'spines_try', temp_outputs_path)
+hair_temp_output_cleaned_csv, hair_temp_output_accepted_csv = generate_temp_output_file_paths(
+    'hairs_try', temp_outputs_path)
 
 spine_powo_search_temp_output_cleaned_csv, spine_powo_search_temp_output_accepted_csv = generate_temp_output_file_paths(
     'spines_powo', temp_outputs_path)
 
+hairs_powo_search_temp_output_cleaned_csv, hairs_powo_search_temp_output_accepted_csv = generate_temp_output_file_paths(
+    'hairs_powo', temp_outputs_path)
+
 ### Outputs
 output_path = resource_filename(__name__, 'outputs')
 spines_output_csv = os.path.join(output_path, 'spines_hits.csv')
+hairy_output_csv = os.path.join(output_path, 'hairy_hits.csv')
+
+#
+# Values for each spine feature which don't indicate spines
+spine_values_to_drop = {'Thorn / spine length': [str(0), 0],
+                        'Spinescence / thorniness': ['spines absent', str(0), 0, 'no', 'Absence'],
+                        'Thorn density': [str(0), 0],
+                        'Leaf spines: upper': ['no'],
+                        'Leaf Spines: lower': ['no'],
+                        'Leaf Spines: midrib/veins': ['no'],
+                        'Leaf Spines: marginal': ['no'],
+                        'Leaf Spines: terminal': ['no'],
+                        'Leaf thorn': ['no'],
+                        'Stem spiny': ['no'],
+                        'Stem thorny': ['no'],
+                        'Other spines/thorns': ['no'],
+                        'Leaf spines': ['no'],
+                        'Thorns elsewhere': ['no']
+                        }
+# Values for each hair feature which doesn't indicate hairs
+hair_values_to_drop = {'Leaf trichome canopy height': [],
+                       'Pubescence: presence and abundance of water-absorbing hairs on leaves (e.g., Bromeliaceae)': [1,
+                                                                                                                      str(1)],
+                       'Pubescence (cottony + - horizontal indumentum)': [1, str(1)],
+                       'Hairs orientation': [],
+                       'Pruinose: above': ['no'],
+                       'Pruinose: below': ['no'],
+                       'Pruinose: midrib/veins': ['no'],
+                       'Pruinose: petiole': ['no'],
+                       'Leaf pruinose': ['no'],
+                       'Leaf hair length': [0, str(0)],
+                       'Leaf hairiness': ['no hairs', 'slightly hairy (few hairs or hairs only along veins)',
+                                          'only as young plant with hairs, old no hairs', 'no'],
+                       'Stem hair length': [0, str(0)],
+                       'Pubescence (ascending hairs or papillae)': [1, str(1)],
+                       'Leaf: trichome density': [],
+                       'Leaf hairiness: top': ['no', 'no hairs', 'only as young plant with hairs', 'other'],
+                       'Leaf hairiness: bottom': ['no', 'no hairs', 'only as young plant with hairs', 'other'],
+                       'Lair hairiness: midrib/veins': ['no'],
+                       'Leaf hairiness: marginal': ['no'],
+                       'Leaf with basal hairs': ['no'],
+                       'Leaf pubescent: top': ['no'],
+                       'Leaf pubescent: bottom': ['no'],
+                       'Leaf pubescent: marginal': ['no'],
+                       'Leaf pubescent: midrib/veins': ['no'],
+                       'Stem hairy': ['no'],
+                       'Stem pubescent': ['no'],
+                       'Leaf pubescence': ['no']
+
+                       }
 
 
 def clean_try_db():
@@ -54,97 +109,111 @@ def clean_try_db():
         raise ValueError('Some items have been lost')
 
 
+def get_accepted_info_hair_hits():
+    emergence_db = pd.read_csv(emergence_input_csv)
+    emergence_db.dropna(subset=['OrigValueStr'], inplace=True)
+
+    # First remove undesired traits (in this case hair)
+    hair_db = emergence_db[~emergence_db['DataName'].isin(spine_values_to_drop.keys())]
+    # Remove unpub works as can't check the meanings of values.
+    hair_db = hair_db[hair_db['Reference'] != 'unpub.']
+
+    # Look at possible values
+    for c in hair_db['DataName'].unique():
+        specific_trait = hair_db[hair_db['DataName'] == c]
+
+        print(c)
+        print(list(specific_trait['OrigValueStr'].unique()))
+
+    for k in hair_values_to_drop:
+        hair_db = hair_db[
+            ~((hair_db['DataName'] == k) & (hair_db['OrigValueStr'].isin(hair_values_to_drop[k])))]
+
+    for i in hair_db['DataName'].unique():
+        if i not in hair_values_to_drop.keys():
+            raise ValueError(f'Unknown hair dataname: {i}')
+
+    for y in ['thorn', 'spiny', 'spine']:
+        if any(y in x for x in list(hair_db['DataName'].unique())):
+            raise ValueError(f'{y} found in Dataname column')
+
+    hair_db.to_csv(hair_temp_output_cleaned_csv)
+    standardise_names_in_column('AccSpeciesName', hair_temp_output_cleaned_csv, hair_temp_output_accepted_csv)
+
+
 def get_accepted_info_spine_hits():
     emergence_db = pd.read_csv(emergence_input_csv)
     emergence_db.dropna(subset=['OrigValueStr'], inplace=True)
+
+    # First remove undesired traits (in this case hair)
+    spine_db = emergence_db[~emergence_db['DataName'].isin(hair_values_to_drop.keys())]
+    # Remove unpub works as can't check the meanings of values.
+    spine_db = spine_db[spine_db['Reference'] != 'unpub.']
+
     # Look at possible values
-    for c in emergence_db['DataName'].unique():
-        specific_trait = emergence_db[emergence_db['DataName'] == c]
+    for c in spine_db['DataName'].unique():
+        specific_trait = spine_db[spine_db['DataName'] == c]
 
         print(c)
         print(specific_trait['OrigValueStr'].unique())
 
-    trs_to_drop = ['Leaf trichome canopy height',
-                   'Pubescence: presence and abundance of water-absorbing hairs on leaves (e.g., Bromeliaceae)',
-                   'Pubescence (cottony + - horizontal indumentum)', 'Hairs orientation', 'Pruinose: above',
-                   'Pruinose: below', 'Pruinose: midrib/veins', 'Pruinose: petiole', 'Leaf pruinose',
-                   'Leaf hair length',
-                   'Leaf hairiness', 'Stem hair length', 'Pubescence (ascending hairs or papillae)',
-                   'Leaf: trichome density',
-                   'Leaf hairiness: top',
-                   'Leaf hairiness: bottom',
-                   'Lair hairiness: midrib/veins',
-                   'Leaf hairiness: marginal',
-                   'Leaf with basal hairs',
-                   'Leaf pubescent: top',
-                   'Leaf pubescent: bottom',
-                   'Leaf pubescent: marginal',
-                   'Leaf pubescent: midrib/veins',
-                   'Stem hairy',
-                   'Stem pubescent',
-                   'Leaf pubescence'
-
-                   ]
-
-    # Values for each feature which don't indicate spines
-    val_to_drop = {'Thorn / spine length': [str(0), 0],
-                   'Spinescence / thorniness': ['spines absent', str(0), 0, 'no', 'Absence'],
-                   'Thorn density': [str(0), 0],
-                   'Leaf spines: upper': ['no'],
-                   'Leaf Spines: lower': ['no'],
-                   'Leaf Spines: midrib/veins': ['no'],
-                   'Leaf Spines: marginal': ['no'],
-                   'Leaf Spines: terminal': ['no'],
-                   'Leaf thorn': ['no'],
-                   'Stem spiny': ['no'],
-                   'Stem thorny': ['no'],
-                   'Other spines/thorns': ['no'],
-                   'Leaf spines': ['no'],
-                   'Thorns elsewhere': ['no']
-                   }
-    # First remove undesired traits (in this case hair)
-    spine_db = emergence_db[~emergence_db['DataName'].isin(trs_to_drop)]
-    # Remove unpub works as can't check the meanings of values.
-    spine_db = spine_db[spine_db['Reference'] != 'unpub.']
-
-    for k in val_to_drop:
+    for k in spine_values_to_drop:
         spine_db = spine_db[
-            ~((spine_db['DataName'] == k) & (spine_db['OrigValueStr'].isin(val_to_drop[k])))]
+            ~((spine_db['DataName'] == k) & (spine_db['OrigValueStr'].isin(spine_values_to_drop[k])))]
 
-    spine_db.to_csv(spine_temp_output_cleaned_csv)
-    standardise_names_in_column('AccSpeciesName', spine_temp_output_cleaned_csv, spine_temp_output_accepted_csv)
-
-def clean_accepted_spine_hits():
-    spine_db = pd.read_csv(spine_temp_output_accepted_csv)
-    spine_db['OrigUnitStr'].fillna('',inplace=True)
-    spine_db['try_Snippet'] = spine_db['DataName'] + ':' + spine_db['OrigValueStr'] + spine_db[
-        'OrigUnitStr']
-    spine_db[COL_NAMES['single_source']] = 'TRY (' + spine_db['Reference'] + ')'
-
+    for i in spine_db['DataName'].unique():
+        if i not in spine_values_to_drop.keys():
+            raise ValueError(f'Unknown spine dataname: {i}')
 
     for y in ['hair', 'pubescence', 'trichome']:
         if any(y in x for x in list(spine_db['DataName'].unique())):
             raise ValueError(f'{y} found in Dataname column')
 
-    spine_db.to_csv(spine_temp_output_accepted_csv)
+    spine_db.to_csv(spine_temp_output_cleaned_csv)
+    standardise_names_in_column('AccSpeciesName', spine_temp_output_cleaned_csv, spine_temp_output_accepted_csv)
+
+
+def clean_accepted_try_hits(temp_output_accepted_csv: str):
+    df = pd.read_csv(temp_output_accepted_csv)
+
+    df['OrigUnitStr'].fillna('', inplace=True)
+    df['try_Snippet'] = df['DataName'] + ':' + df['OrigValueStr'] + df[
+        'OrigUnitStr']
+    df[COL_NAMES['single_source']] = 'TRY (' + df['Reference'] + ')'
+
+    df.to_csv(temp_output_accepted_csv)
+
 
 def get_powo_spine_hits():
     search_powo('spine,thorn,spike', spine_powo_search_temp_output_cleaned_csv,
                 spine_powo_search_temp_output_accepted_csv)
 
+def get_powo_hair_hits():
+    search_powo('hairs,hairy', hairs_powo_search_temp_output_cleaned_csv,
+                hairs_powo_search_temp_output_accepted_csv)
+
 
 def main():
-    # clean_try_db()
-    # get_accepted_info_spine_hits()
-    clean_accepted_spine_hits()
+    clean_try_db()
+    get_accepted_info_spine_hits()
+    clean_accepted_try_hits(spine_temp_output_accepted_csv)
+
     # get_powo_spine_hits()
 
-    powo_hits = pd.read_csv(spine_powo_search_temp_output_accepted_csv)
-    try_hits = pd.read_csv(spine_temp_output_accepted_csv)
+    powo_spine_hits = pd.read_csv(spine_powo_search_temp_output_accepted_csv)
+    try_spine_hits = pd.read_csv(spine_temp_output_accepted_csv)
 
-    all_dfs = [try_hits, powo_hits]
-    compile_hits(all_dfs, spines_output_csv)
+    all_spine_dfs = [try_spine_hits, powo_spine_hits]
+    compile_hits(all_spine_dfs, spines_output_csv)
 
+    get_accepted_info_hair_hits()
+    clean_accepted_try_hits(hair_temp_output_accepted_csv)
+    get_powo_hair_hits()
 
+    powo_hair_hits = pd.read_csv(hairs_powo_search_temp_output_accepted_csv)
+    try_hair_hits = pd.read_csv(hair_temp_output_accepted_csv)
+
+    all_hair_hits = [powo_hair_hits,try_hair_hits]
+    compile_hits(all_hair_hits,hairy_output_csv)
 if __name__ == '__main__':
     main()
