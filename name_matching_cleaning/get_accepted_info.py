67c63c52e1ca13f04cc27049f48fa521fdb97411
alrichardbollans <adamrb@protonmail.com>
2022-01-27 17:28:12 +0000

    Develop accepted naming

--- name_matching_cleaning/get_accepted_info.py
+++ name_matching_cleaning/get_accepted_info.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 
 import numpy as np
 import pykew.powo as powo
@@ -82,14 +83,13 @@ def get_accepted_info_from_ids_in_column(all_taxa: pd.DataFrame, df: pd.DataFram
     dict_of_values = {'Accepted_Name': [], 'Accepted_ID': [], 'Accepted_Rank': [],
                       'Accepted_Species': [], 'Accepted_Species_ID': []}
     [id_lookup_wcvp(all_taxa, x, dict_of_values) for x in df[id_col_name].values]
-    print(dict_of_values)
 
     match_df = pd.DataFrame(dict_of_values)
 
     return pd.concat([df, match_df], axis=1)
 
 
-def find_best_match(multiple_match_records: pd.DataFrame, families_of_interest=None) -> pd.DataFrame:
+def find_best_matches(multiple_match_records: pd.DataFrame, families_of_interest=None) -> pd.DataFrame:
     """
     Gets best matching for each submitted match and returns a dataframe of single matches
     """
@@ -98,42 +98,63 @@ def find_best_match(multiple_match_records: pd.DataFrame, families_of_interest=N
     multiple_matches_with_accepted_ids = get_accepted_info_from_ids_in_column(wcvp_taxa, multiple_match_records,
                                                                               'ipni_id')
 
+    # Matches where accepted name is the same as the submitted name
+    accepted_names_matching_submitted_names = multiple_matches_with_accepted_ids[
+        multiple_matches_with_accepted_ids['submitted'] == multiple_matches_with_accepted_ids['Accepted_Name']]
 
-    pass
-    # match_df['submitted'] = df['submitted']
-    #
-    # accepted_names_matching_submitted_names = match_df[match_df['submitted'] == match_df['Accepted_Name']]
-    # submitted_names_with_single_accepted_match = match_df.drop_duplicates(subset=['submitted'], keep=False)
-    #
-    # matches_to_use = pd.concat([accepted_names_matching_submitted_names, submitted_names_with_single_accepted_match])
-    # matches_to_use.drop_duplicates('Accepted_ID',inplace=True)
+    # Matches where submission differs or submission is the same and accepted ids differ
+    unique_matches = multiple_matches_with_accepted_ids.drop_duplicates(
+        subset=['submitted', 'Accepted_ID'], keep='first')
+
+    # Matches where the submitted name has a unique accepted match
+    submitted_names_with_single_accepted_match = unique_matches.drop_duplicates(subset=['submitted'],keep=False)
+
+    matches_to_use = pd.concat([accepted_names_matching_submitted_names, submitted_names_with_single_accepted_match])
+    matches_to_use.drop_duplicates(subset=['submitted'], keep='first', inplace=True)
+
+    return matches_to_use
 
 
-def knms_match_names(names: List[str]):
-    knms_url = "http://namematch.science.kew.org/api/v2/powo/match"
-    res = requests.post(knms_url, json=names)
-    headings = ['submitted', 'match_state', 'ipni_id', 'matched_name']
+def get_knms_name_matches(names: List[str]):
+    names = list(names)
+    temp_csv = str(hash(str(names))) +".csv"
+    print(temp_csv)
+    temp_output_knms_csv = os.path.join(temp_outputs,temp_csv)
+    if os.path.isfile(temp_output_knms_csv):
+        records = pd.read_csv(temp_output_knms_csv)
+    else:
+        knms_url = "http://namematch.science.kew.org/api/v2/powo/match"
+        res = requests.post(knms_url, json=names)
+        headings = ['submitted', 'match_state', 'ipni_id', 'matched_name']
 
-    content = json.loads(res.content.decode('utf-8'))
-    records = pd.DataFrame(content["records"], columns=headings)
-    records.replace('', np.nan, inplace=True)
-    records['submitted'].ffill(inplace=True)
-    records['match_state'].ffill(inplace=True)
+        content = json.loads(res.content.decode('utf-8'))
+        records = pd.DataFrame(content["records"], columns=headings)
+        records.replace('', np.nan, inplace=True)
+        records['submitted'].ffill(inplace=True)
+        records['match_state'].ffill(inplace=True)
 
-    records.to_csv('allm.csv')
-    multiple_matches = records[records['match_state'] == 'multiple_matches']
-    multiple_matches.to_csv('testmm.csv')
-    print(res)
+        records.to_csv(temp_output_knms_csv)
+
+    return records
+
 
 
 def get_matched_names_from_names_in_column(df: pd.DataFrame, name_col: str):
+    match_records = get_knms_name_matches(df[name_col].values)
+    multiple_matches = match_records[match_records['match_state'] == 'multiple_matches']
+
+    best_matches = find_best_matches(multiple_matches)
+
+
     pass
 
 
 if __name__ == '__main__':
+    test_data = pd.read_csv('input/spines_genera_list_DG.csv')
+    get_matched_names_from_names_in_column(test_data,'Labelled')
     # knms_match_names(['Poa annua','Aspidosperma'])
-    mm = pd.read_csv('testmm.csv')
-    print(mm.columns)
-    find_best_match(mm)
+    # mm = pd.read_csv('testmm.csv')
+    # print(mm.columns)
+    # find_best_matches(mm)
     # id_lookup_powo('44583-2')
     # id_lookup_wcvp('77210192-1')
