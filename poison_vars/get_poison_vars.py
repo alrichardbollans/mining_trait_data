a2f4dae4e25250554c080200a67e96bb4b7039be
alrichardbollans <adamrb@protonmail.com>
2022-02-22 12:06:50 +0000

    Add cornell poisons

--- poison_vars/get_poison_vars.py
+++ poison_vars/get_poison_vars.py
@@ -15,6 +15,7 @@ _inputs_path = resource_filename(__name__, 'inputs')
 _input_species_csv = os.path.join(_inputs_path, 'clean.csv')
 
 _useful_plants_file = os.path.join(_inputs_path, 'useful_plant_processed_db.txt')
+_cornell_file = os.path.join(_inputs_path, 'Plants Poisonous to Livestock Cornell University.html')
 
 ### Temp outputs
 _temp_outputs_path = resource_filename(__name__, 'temp_outputs')
@@ -23,10 +24,19 @@ _useful_temp_output_accepted_csv = os.path.join(_temp_outputs_path, 'useful_acce
 _powo_search_temp_output_accepted_csv = os.path.join(_temp_outputs_path, 'powo_poisons_accepted.csv')
 _wiki_search_temp_output_csv = os.path.join(_temp_outputs_path, 'wiki_poisons.csv')
 _wiki_search_temp_output_accepted_csv = os.path.join(_temp_outputs_path, 'wiki_poisons_accepted.csv')
+_cornell_temp_accepted_csv = os.path.join(_temp_outputs_path, 'cornell_accepted.csv')
 
 ### Outputs
 _output_path = resource_filename(__name__, 'outputs')
 output_poison_csv = os.path.join(_output_path, 'list_of_poisonous_plants.csv')
+
+
+def prepare_cornell_data() -> pd.DataFrame:
+    tables = pd.read_html(_cornell_file)
+    tables[0]['Source'] = 'Cornell CALS'
+    db_acc = get_accepted_info_from_names_in_column(tables[0], 'Scientific Name')
+    db_acc.to_csv(_cornell_temp_accepted_csv)
+    return db_acc
 
 
 def prepare_useful_plants_poisons() -> pd.DataFrame:
@@ -87,14 +97,17 @@ def main():
     # get_powo_poisons()
     # prepare_littox_poisons()
     # prepare_useful_plants_poisons()
+    prepare_cornell_data()
+    cornell_hits = pd.read_csv(_cornell_temp_accepted_csv)
     wiki_hits = pd.read_csv(_wiki_search_temp_output_accepted_csv)
     powo_hits = pd.read_csv(_powo_search_temp_output_accepted_csv)
     littox_hits = pd.read_csv(_littox_temp_output_accepted_csv)
     littox_hits['Source'] = 'LITTOX'
     useful_hits = pd.read_csv(_useful_temp_output_accepted_csv)
     useful_hits['Accepted_Species_ID'] = useful_hits['Accepted_ID']
-    compile_hits([useful_hits, powo_hits, littox_hits, wiki_hits], output_poison_csv)
+    compile_hits([useful_hits, powo_hits, littox_hits, wiki_hits, cornell_hits], output_poison_csv)
 
 
 if __name__ == '__main__':
     main()
+    # prepare_cornell_data()
