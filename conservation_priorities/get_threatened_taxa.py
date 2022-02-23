82136b7388d8e3a0e21edc543b63613b854a9d35
alrichardbollans <adamrb@protonmail.com>
2022-02-23 16:32:38 +0000

    Add threatened taxa methods

--- conservation_priorities/get_threatened_taxa.py
+++ conservation_priorities/get_threatened_taxa.py
@@ -1,20 +1,33 @@
+import os
+
 import requests
 from typing import List
 import pandas as pd
-test_url ='https://tools.bgci.org/export_threatsearch.php?ftrFamily=&ftrGenus=Aspidosperma&ftrSpecies=&ftrInfraSpecName=&ftrBGCI_Scope=&ftrAssessmentYear=&ftrThreatened=Threatened&ftrPagerLimit=100000&action=Find&export=1'
+from pkg_resources import resource_filename
 
-base_url = 'https://tools.bgci.org/export_threatsearch.php?ftrFamily=&ftrGenus=GENUS&ftrSpecies=&ftrInfraSpecName=&ftrBGCI_Scope=&ftrAssessmentYear=&ftrThreatened=Threatened&ftrPagerLimit=100000&action=Find&export=1'
+from taxa_lists import get_all_taxa
 
-def get_threatened_taxa_in_genus(genus):
-    url_format = base_url.replace('GENUS',genus)
-    p= pd.read_csv(url_format)
-    pass
+_base_url = 'https://tools.bgci.org/export_threatsearch.php?ftrFamily=&ftrGenus=GENUS&ftrSpecies=&ftrInfraSpecName=&ftrBGCI_Scope=&ftrAssessmentYear=&ftrThreatened=Threatened&ftrPagerLimit=100000&action=Find&export=1'
 
-def get_threatened_taxa(taxa_list :List[str],):
-    pass
+_outputs_path = resource_filename(__name__, 'outputs')
+rub_apoc_threatened_csv = os.path.join(_outputs_path, 'rub_apocs_threatened.csv')
+
+def get_threatened_taxa_in_genus(genus)->pd.DataFrame:
+    # Note we don't search by family families are commonly not given in the bgci data
+    url_format = _base_url.replace('GENUS', genus)
+    return pd.read_csv(url_format)
+
+def get_threatened_taxa_in_family(families :List[str])->pd.DataFrame:
+    taxa = get_all_taxa(families_of_interest=families, accepted=True)
+    genera_dfs = []
+    for genus in taxa['genus'].unique().values:
+        genera_dfs.append(get_threatened_taxa_in_genus(genus))
+
+    return pd.concat(genera_dfs)
+
 
 
 def main():
-    get_threatened_taxa_in_genus('Aspidosperma')
+    get_threatened_taxa_in_family(['Apocynaceae', 'Rubiaceae']).to_csv(rub_apoc_threatened_csv)
 if __name__ == '__main__':
     main()
