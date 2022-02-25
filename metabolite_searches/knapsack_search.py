a15d4fa52521d42568a7de41f285fefcf6dcf70d
alrichardbollans <adamrb@protonmail.com>
2022-02-25 14:47:41 +0000

    Seperate methods and variables

--- metabolite_searches/knapsack_search.py
+++ metabolite_searches/knapsack_search.py
@@ -17,11 +17,6 @@ _inputs_path = resource_filename(__name__, 'inputs')
 _temp_outputs_path = resource_filename(__name__, 'temp_outputs')
 _check_csv = os.path.join(_temp_outputs_path, 'taxa_to_recheck.csv')
 _outputs_path = resource_filename(__name__, 'outputs')
-rubiaceae_apocynaceae_metabolites_output_csv = os.path.join(_outputs_path, 'rub_apocs_metabolites.csv')
-rubiaceae_apocynaceae_alks_output_csv = os.path.join(_outputs_path, 'rub_apocs_alkaloids.csv')
-rub_apoc_alkaloid_hits_output_csv = os.path.join(_outputs_path, 'rub_apocs_alkaloid_hits.csv')
-rub_apoc_antibac_metabolites_output_csv = os.path.join(_outputs_path, 'rub_apocs_antibac_metabolites.csv')
-_check_output_csv = os.path.join(_outputs_path, 'rechecked_taxa.csv')
 
 
 def get_metabolites_for_taxon(name: str):
@@ -104,18 +99,10 @@ def get_metabolites_for_taxa(taxa_list: List[str], output_csv: str = None, force
     return acc_df
 
 
-def recheck_taxa():
+def recheck_taxa(check_output_csv: str):
     taxa = pd.read_csv(_check_csv)
     taxa_list = taxa['taxa'].values
-    get_metabolites_for_taxa(taxa_list, output_csv=_check_output_csv)
-
-
-def summarise_metabolites():
-    metas_data = pd.read_csv(rubiaceae_apocynaceae_metabolites_output_csv)
-    summ = metas_data.describe()
-    print(summ)
-    worthwhile_metabolites = [x for x in summ.columns if summ[x]['mean'] > 0.001]
-    print(worthwhile_metabolites)
+    get_metabolites_for_taxa(taxa_list, output_csv=check_output_csv)
 
 
 def get_antibac_metabolite_hits_for_taxa(taxa_metabolite_data: pd.DataFrame, output_csv: str, fams: List[str] = None):
@@ -206,40 +193,8 @@ def get_alkaloid_hits_for_taxa(taxa_metabolite_data: pd.DataFrame, alkaloid_df: 
     return acc_df
 
 
-def get_rub_apoc_antibac_metabolite_hits():
-    all_metas_data = pd.read_csv(rubiaceae_apocynaceae_metabolites_output_csv)
-    get_antibac_metabolite_hits_for_taxa(all_metas_data, rub_apoc_antibac_metabolites_output_csv,
-                                         fams=['Rubiaceae', 'Apocynaceae'])
-
-
-def get_rub_apoc_metabolites():
-    data = get_all_taxa(families_of_interest=['Apocynaceae', 'Rubiaceae'], accepted=True)
-
-    ranks_to_use = ["Species", "Variety", "Subspecies"]
-
-    taxa = data.loc[data["rank"].isin(ranks_to_use)]
-
-    taxa_list = taxa["taxon_name"].values
-
-    get_metabolites_for_taxa(taxa_list, output_csv=rubiaceae_apocynaceae_metabolites_output_csv)
-
-
-def get_rub_apoc_alkaloid_hits():
-    metabolites_to_check = pd.read_csv(rubiaceae_apocynaceae_metabolites_output_csv).columns.tolist()
-
-    alks_df = output_alkaloids_from_metabolites(metabolites_to_check, rubiaceae_apocynaceae_alks_output_csv)
-
-    rubs_apoc_metas_data = pd.read_csv(rubiaceae_apocynaceae_metabolites_output_csv)
-    get_alkaloid_hits_for_taxa(rubs_apoc_metas_data, alks_df, rub_apoc_alkaloid_hits_output_csv,
-                               fams=['Rubiaceae', 'Apocynaceae'])
-
-
 def main():
-    get_rub_apoc_metabolites()
-    recheck_taxa()
-    summarise_metabolites()
-    get_rub_apoc_antibac_metabolite_hits()
-    get_rub_apoc_alkaloid_hits()
+    pass
 
 
 if __name__ == '__main__':
---
+++ metabolite_vars/__init__.py
@@ -0,0 +1,1 @@
+from .get_rub_apoc_metabolite_vars import *
