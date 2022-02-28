f13c62e1a520c74010fc15bf0402f605bcb3a7ed
alrichardbollans <adamrb@protonmail.com>
2022-02-28 16:10:55 +0000

    Add batching

--- cleaning/preparing_datasets.py
+++ cleaning/preparing_datasets.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import pandas as pd
 from automatchnames import get_accepted_info_from_names_in_column
 from typing import List
@@ -10,9 +11,20 @@ def get_tempout_csv(dataset_name: str, temp_output_path: str):
 
 
 def generic_prepare_data(dataset_name: str, temp_output_path: str, df: pd.DataFrame, name_col: str,
-                         dropifna: List[str] = None, families_of_interest: List[str]=None):
+                         dropifna: List[str] = None, families_of_interest: List[str] = None, batch: bool = False):
     if dropifna is not None:
         df.dropna(subset=dropifna, inplace=True)
     df['Source'] = dataset_name
-    db_acc = get_accepted_info_from_names_in_column(df, name_col,families_of_interest=families_of_interest)
+
+    if batch:
+        n = int(len(df.index)/ 1000)+1
+        list_df = np.array_split(df, n)
+        acc_dfs = []
+        for split_df in list_df:
+            print(split_df)
+            acc_dfs.append(
+                get_accepted_info_from_names_in_column(split_df, name_col, families_of_interest=families_of_interest))
+        db_acc = pd.concat(acc_dfs)
+    else:
+        db_acc = get_accepted_info_from_names_in_column(df, name_col, families_of_interest=families_of_interest)
     db_acc.to_csv(get_tempout_csv(dataset_name, temp_output_path))
