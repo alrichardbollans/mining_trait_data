0a8bdd2a69aeaa73f9565dbb77d1cc2bef3b34d8
alrichardbollans <adamrb@protonmail.com>
2022-03-14 09:49:21 +0000

    Add ph

--- soil_vars/get_soil_vars.py
+++ soil_vars/get_soil_vars.py
@@ -9,10 +9,11 @@ from tqdm import tqdm
 
 tqdm.pandas()
 
-from plant_occurence_vars import apoc_rub_occurences_csv
+from plant_occurence_searches import cleaned_apoc_rub_occurences_csv
 
 _output_path = resource_filename(__name__, 'outputs')
-apocs_rubs_nitrogen_means = os.path.join(_output_path,'apocs_rubs_nitrogen_soil_means.csv')
+apocs_rubs_nitrogen_means = os.path.join(_output_path, 'apocs_rubs_nitrogen_soil_means.csv')
+apocs_rubs_ph_means = os.path.join(_output_path, 'apocs_rubs_ph_soil_means.csv')
 
 
 def get_mean_property_for_lat_long(lon: float, lat: float, prop: str):
@@ -31,7 +32,7 @@ def add_property_means(df_of_occurences: pd.DataFrame, prop: str):
     # Only search for unique cases
     unique_longs_and_lats = df_of_occurences.drop_duplicates(subset=['decimalLongitude', 'decimalLatitude'])
     unique_longs_and_lats[prop] = unique_longs_and_lats.progress_apply(get_mean_property_for_row, axis=1,
-                                                                             prop=prop)
+                                                                       prop=prop)
     unique_longs_and_lats = unique_longs_and_lats[['decimalLongitude', 'decimalLatitude', prop]]
     # Merge into original
     out_df = df_of_occurences.merge(unique_longs_and_lats, on=['decimalLongitude', 'decimalLatitude'],
@@ -39,7 +40,19 @@ def add_property_means(df_of_occurences: pd.DataFrame, prop: str):
 
     return out_df
 
-if __name__ == "__main__":
-    occ_df =pd.read_csv(apoc_rub_occurences_csv)
-    with_nitrogen = add_property_means(occ_df,'nitrogen')
+
+def _get_nitrogen_means():
+    occ_df = pd.read_csv(cleaned_apoc_rub_occurences_csv, index_col=0)
+    with_nitrogen = add_property_means(occ_df, 'nitrogen')
     with_nitrogen.to_csv(apocs_rubs_nitrogen_means)
+
+
+def _get_ph_means():
+    occ_df = pd.read_csv(cleaned_apoc_rub_occurences_csv, index_col=0)
+    with_ph = add_property_means(occ_df, 'phh2o')
+    with_ph.to_csv(apocs_rubs_ph_means)
+
+
+if __name__ == "__main__":
+    _get_nitrogen_means()
+    _get_ph_means()
