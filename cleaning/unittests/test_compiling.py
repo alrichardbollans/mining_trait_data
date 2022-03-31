7906cf52b062634cb62c37d117947b3e9f045db8
alrichardbollans <adamrb@protonmail.com>
2022-03-31 15:29:59 +0100

    Test merging

--- cleaning/unittests/test_compiling.py
+++ cleaning/unittests/test_compiling.py
@@ -5,7 +5,7 @@ import pandas as pd
 from automatchnames import COL_NAMES
 from pkg_resources import resource_filename
 
-from cleaning import filter_out_ranks
+from cleaning import filter_out_ranks, generate_temp_output_file_paths, merge_columns, merge_on_accepted_id
 
 _inputs_path = resource_filename(__name__, 'test_inputs')
 
@@ -16,6 +16,18 @@ class MyTestCase(unittest.TestCase):
         filtered = filter_out_ranks(t_df)
         self.assertGreater(len(filtered.index), 0)
         self.assertEqual(filtered[COL_NAMES['acc_rank']].unique(), ['Species'])
+
+    def test_merge_columns(self):
+        t_df = pd.read_csv(os.path.join(_inputs_path, 'to_merge.csv'))
+        merged = merge_columns(t_df, 'auto_merged', [COL_NAMES['acc_name'], COL_NAMES['acc_id']])
+        pd.testing.assert_series_equal(merged['auto_merged'], t_df['Merged'], check_names=False)
+
+    def test_merge_on_accepted_id(self):
+        merged_df = pd.read_csv(os.path.join(_inputs_path, 'id_merged.csv'))
+        one_df = pd.read_csv(os.path.join(_inputs_path, 'to_merge_id1.csv'))
+        two_df = pd.read_csv(os.path.join(_inputs_path, 'to_merge_id2.csv'))
+        automerged = merge_on_accepted_id(one_df,two_df)
+        pd.testing.assert_frame_equal(merged_df,automerged,check_exact=False)
 
 
 if __name__ == '__main__':
