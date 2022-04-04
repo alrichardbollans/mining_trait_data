62fc72cd07b808700fe70fe74c4e01a8f3dc077c
alrichardbollans <adamrb@protonmail.com>
2022-04-04 17:37:05 +0100

    Include raster import methods

---
+++ climate_vars/append_from_rasters.R
@@ -0,0 +1,21 @@
+library(raster)
+
+import_raster <- function(filename){
+  str_name<-paste('inputs/environmental_variables/',filename,sep='')
+  imported_raster=raster(str_name)
+  return(imported_raster)
+}
+
+append_var_to_df <- function(df,var_name){
+  x = import_raster(var_name)
+  var_values = data.frame(extract(stack(x), df[, c("decimalLongitude", "decimalLatitude")]))
+  return(cbind(df, var_values))
+}
+
+
+# Get dataframe containing cleaned occurences
+species_df = read.csv(file.path('inputs','cleaned_species_occurences.csv'))
+bioclim_df <- species_df
+
+bioclim_df<-append_var_to_df(bioclim_df,'SoilpH.tif')
+bioclim_df<-append_var_to_df(bioclim_df,'SoilpH.tif')
