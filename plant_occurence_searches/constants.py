5d85d0b6e5a6ffe47639339cfb2ff4d49dab04c0
alrichardbollans <adamrb@protonmail.com>
2022-04-05 16:40:12 +0100

    Update download location

--- plant_occurence_searches/constants.py
+++ plant_occurence_searches/constants.py
@@ -4,4 +4,3 @@ from pkg_resources import resource_filename
 
 # _output_path = resource_filename(__name__, 'outputs')
 # apoc_rub_occurences_csv = os.path.join(_output_path,'apocs_rubs_occurences.csv')
-cleaned_apoc_rub_occurences_csv = '/home/atp/Documents/work life/Kew/large folders/plant_occurence_vars/outputs/cleaned_species_occurences.csv'
