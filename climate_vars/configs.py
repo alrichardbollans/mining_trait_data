23cdb0712c2f37a2df9dedb3b36e581a4345b73f
alrichardbollans <adamrb@protonmail.com>
2022-04-05 10:25:52 +0100

    Tidy names

--- climate_vars/configs.py
+++ climate_vars/configs.py
@@ -2,17 +2,17 @@ import os
 
 from pkg_resources import resource_filename
 
-_inputs_path = resource_filename(__name__, 'inputs')
-spec_occurenc_input = os.path.join(_inputs_path, 'cleaned_species_occurences.csv')
+climate_inputs_path = resource_filename(__name__, 'inputs')
+spec_occurence_input = os.path.join(climate_inputs_path, 'cleaned_species_occurences.csv')
 
-_temp_outputs_path = resource_filename(__name__, 'temp_outputs')
-spec_elevations_csv = os.path.join(_temp_outputs_path, 'cleaned_species_occurences_w_elevation.csv')
+climate_temp_outputs_path = resource_filename(__name__, 'temp_outputs')
 
-_output_path = resource_filename(__name__, 'outputs')
-summary_output_csv = os.path.join(_output_path,'occurence_summary.csv')
-if not os.path.isdir(_inputs_path):
-    os.mkdir(_inputs_path)
-if not os.path.isdir(_temp_outputs_path):
-    os.mkdir(_temp_outputs_path)
-if not os.path.isdir(_output_path):
-    os.mkdir(_output_path)
+
+climate_output_path = resource_filename(__name__, 'outputs')
+summary_output_csv = os.path.join(climate_output_path,'occurence_summary.csv')
+if not os.path.isdir(climate_inputs_path):
+    os.mkdir(climate_inputs_path)
+if not os.path.isdir(climate_temp_outputs_path):
+    os.mkdir(climate_temp_outputs_path)
+if not os.path.isdir(climate_output_path):
+    os.mkdir(climate_output_path)
--- climate_vars/summarise_occurences.py
+++ climate_vars/summarise_occurences.py
@@ -6,31 +6,12 @@ from pkg_resources import resource_filename
 from typing import List
 from taxa_lists import get_all_taxa
 
-from climate_vars import spec_occurenc_input, summary_output_csv
-
-### Inputs
-_inputs_path = resource_filename(__name__, 'inputs')
-_clean_occurence_input_csv = os.path.join(_inputs_path, 'cleaned_species_occurences.csv')
-
-### Temp outputs
-_temp_outputs_path = resource_filename(__name__, 'temp_outputs')
-
-### Outputs
-_output_path = resource_filename(__name__, 'outputs')
-
-if not os.path.isdir(_inputs_path):
-    os.mkdir(_inputs_path)
-if not os.path.isdir(_temp_outputs_path):
-    os.mkdir(_temp_outputs_path)
-if not os.path.isdir(_output_path):
-    os.mkdir(_output_path)
-
-
+from climate_vars import spec_occurence_input, summary_output_csv
 
 
 def summarise():
     acc_sp = get_all_taxa(families_of_interest=['Apocynaceae', 'Rubiaceae'], accepted=True, ranks=['Species'])
-    clean_occ_df = pd.read_csv(spec_occurenc_input)
+    clean_occ_df = pd.read_csv(spec_occurence_input)
     unique_species = clean_occ_df['species'].unique()
 
 
