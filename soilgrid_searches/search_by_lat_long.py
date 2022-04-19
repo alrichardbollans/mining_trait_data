ca742de23e083fa51ca62960fed9bb0dda51013f
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-04-19 16:05:12 +0100

    Generalise methods

--- soilgrid_searches/search_by_lat_long.py
+++ soilgrid_searches/search_by_lat_long.py
@@ -1,24 +1,15 @@
-import os
+import time
 
 import pandas as pd
 import requests
-from pkg_resources import resource_filename
-
 # Add progress bar to apply method
 from tqdm import tqdm
-
-from climate_vars import spec_occurence_input
 
 tqdm.pandas()
 
 
-_output_path = resource_filename(__name__, 'outputs')
-apocs_rubs_nitrogen_means = os.path.join(_output_path, 'apocs_rubs_nitrogen_soil_means.csv')
-apocs_rubs_ph_means = os.path.join(_output_path, 'apocs_rubs_ph_soil_means.csv')
-
-
 def get_mean_property_for_lat_long(lon: float, lat: float, prop: str):
-    # TODO: add rate limiting protection
+    time.sleep(0.01)
     response = requests.get(
         f'https://rest.isric.org/soilgrids/v2.0/properties/query?lon={lon}&lat={lat}&property={prop}&depth=0-5cm&value=mean').json()
 
@@ -40,20 +31,3 @@ def add_property_means(df_of_occurences: pd.DataFrame, prop: str):
                                     how='left').set_index(df_of_occurences.index)
 
     return out_df
-
-
-def _get_nitrogen_means(occurence_input: str, output_csv: str):
-    occ_df = pd.read_csv(occurence_input, index_col=0)
-    with_nitrogen = add_property_means(occ_df, 'nitrogen')
-    with_nitrogen.to_csv(output_csv)
-
-
-def _get_ph_means(occurence_input: str, output_csv: str):
-    occ_df = pd.read_csv(occurence_input, index_col=0)
-    with_ph = add_property_means(occ_df, 'phh2o')
-    with_ph.to_csv(output_csv)
-
-
-if __name__ == "__main__":
-    _get_nitrogen_means(spec_occurence_input,apocs_rubs_nitrogen_means)
-    _get_ph_means(spec_occurence_input,apocs_rubs_ph_means)
