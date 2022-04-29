ecef3b303f672ad2ea43dfe813c7b8546303d442
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-04-29 10:12:18 +0100

    Move priority method uses

--- conservation_priorities/get_threatened_taxa.py
+++ conservation_priorities/get_threatened_taxa.py
@@ -67,16 +67,3 @@ def interpret_status(threat_df: pd.DataFrame):
             print('########')
             print(species)
             print(sp_threats['Interpreted Conservation Status'].unique())
-
-
-def main():
-    # TODO:Note There are quite a few synonyms of unplaced names
-    # TODO:Note cases like Adenium boehmianum Schinz 	where same report gives threatend and not threatened
-    fams = ['Apocynaceae', 'Rubiaceae']
-    get_threat_status_taxa_in_family(fams, rub_apoc_threat_status_csv, rub_apoc_accepted_threat_status_csv,
-                                     ranks=['Species'])
-
-
-if __name__ == '__main__':
-    interpret_status(pd.read_csv(rub_apoc_accepted_threat_status_csv))
-    # main()
