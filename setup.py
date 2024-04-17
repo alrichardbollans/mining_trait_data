26cf343857026c797eb856b16cf303ff50189e7b
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-04-17 11:50:30 +0100

    Fix install

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author_email='38588335+alrichardbollans@users.noreply.github.com',
     # Needed to actually package something
     packages=find_packages(include=['clean_plant_occurrences', 'data_compilation_methods',
-                                    'knapsack_searches', 'metabolite_properties',
                                     'powo_searches',
                                     'wikipedia_searches'], exclude=['unit_test_methods']),
 
